552db239deea747daa8dc379fc7eb9fe4970981d
@FranCarn <carnevalefranco99@gmail.com>
2024-02-08 17:25:48 -0300

    lifecycles

--- src/modules/pokemon/pages/AboutPage.vue
+++ src/modules/pokemon/pages/AboutPage.vue
@@ -1,3 +1,63 @@
 <template>
   <h1>About Page</h1>
+  <hr />
+  <h2>{{ name }}</h2>
+  <button @click="onChangeName">Change Name</button>
 </template>
+<script>
+export default {
+  data() {
+    return {
+      name: "Test",
+    };
+  },
+  methods: {
+    onChangeName() {
+      this.name = "Updated";
+    },
+  },
+  beforeCreate() {
+    console.log("beforeCreate");
+  },
+  created() {
+    console.log("created");
+    // HTTP resources
+    this.name = "Hello world";
+  },
+  beforeMount() {
+    console.log("beforeMount");
+  },
+  mounted() {
+    console.log("mounted");
+  },
+  beforeUpdate() {
+    console.log("beforeUpdate");
+  },
+  updated() {
+    console.log("updated");
+  },
+  activated() {
+    console.log("activated");
+  },
+  deactivated() {
+    console.log("deactivated");
+  },
+  beforeUnmount() {
+    console.log("beforeUnmount");
+    // cleanup
+  },
+  unmounted() {
+    console.log("unmounted");
+    // cleanup
+  },
+  errorCaptured() {
+    console.log("errorCaptured");
+  },
+  renderTracked() {
+    console.log("renderTracked");
+  },
+  renderTriggered() {
+    console.log("renderTriggered");
+  },
+};
+</script>
