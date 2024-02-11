32248003890b689e3f5d4d9742feb3aa7f6d3351
@FranCarn <carnevalefranco99@gmail.com>
2024-02-11 13:23:02 -0300

    child routes ready

--- src/router/router.js
+++ src/router/router.js
@@ -3,38 +3,49 @@ import { createRouter, createWebHashHistory } from "vue-router";
 const routes = [
   {
     path: "/",
-    redirect: "/home",
+    redirect: "/pokemon",
   },
   {
-    path: "/home",
-    name: "home",
-    component: () =>
-      import(
-        /*webpackChunkName: "ListPage"*/ "../modules/pokemon/pages/ListPage"
-      ),
-  },
-  {
-    path: "/about",
-    name: "about",
-    component: () =>
-      import(
-        /*webpackChunkName: "AboutPage"*/ "../modules/pokemon/pages/AboutPage"
-      ),
-  },
-  {
-    path: "/pokemonId/:pokemonid",
-    component: () =>
-      import(
-        /*webpackChunkName: "PokemonPage"*/ "../modules/pokemon/pages/PokemonPage"
-      ),
-    props: (route) => {
-      const { pokemonid } = route.params;
-      const id = Number(pokemonid);
-      return {
-        id: isNaN(id) ? 1 : id,
-      };
-    },
-    name: "pokemon-id",
+    path: "/pokemon",
+    name: "pokemon",
+    component: () => import("../modules/pokemon/layouts/PokemonLayout"),
+    children: [
+      {
+        path: "home",
+        name: "pokemon-home",
+        component: () =>
+          import(
+            /*webpackChunkName: "ListPage"*/ "../modules/pokemon/pages/ListPage"
+          ),
+      },
+      {
+        path: "about",
+        name: "pokemon-about",
+        component: () =>
+          import(
+            /*webpackChunkName: "AboutPage"*/ "../modules/pokemon/pages/AboutPage"
+          ),
+      },
+      {
+        path: "pokemonId/:pokemonid",
+        component: () =>
+          import(
+            /*webpackChunkName: "PokemonPage"*/ "../modules/pokemon/pages/PokemonPage"
+          ),
+        props: (route) => {
+          const { pokemonid } = route.params;
+          const id = Number(pokemonid);
+          return {
+            id: isNaN(id) ? 1 : id,
+          };
+        },
+        name: "pokemon-id",
+      },
+      {
+        path: "",
+        redirect: { name: "pokemon-about" },
+      },
+    ],
   },
   {
     path: "/:pathMatch(.*)*",
